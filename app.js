15f0dfc6ccf7e4b4c8c083abe7d56ccaf202b454
CaritoDasilva <caritodasilva@gmail.com>
2018-05-26 14:54:37 -0400

    subiendo tarea de funciones matematicas a la cloud

---
+++ app.js
@@ -0,0 +1,16 @@
+function add() {
+addResult = x + y;
+}
+
+function multiply() {
+multiplyResult = addResult * z;
+    alert("Resultado de  36325 * (9824 + 777) es igual a " + multiplyResult);
+}
+let addResult;
+let multiplyResult;
+let x = 9824;
+let y = 777;
+let z = 36325;
+
+add();
+multiply();
